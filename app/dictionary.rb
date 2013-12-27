d0541bf7df93178d93d1c88af78d13136479cf61
Василий Потапов <potapov@che.sevcred.ru>
2013-12-27 13:40:12 +0400

    Some cosmetic improvements

--- app/dictionary.rb
+++ app/dictionary.rb
@@ -7,30 +7,32 @@ class Dictionary
   end
 
   def get_records(dict_name)
-    @records = Hash.new
+    @get_records = Hash.new
 
     config = get_config(dict_name)
 
     config['dictionaries'].each do |dictionary_key, dictionary_value|
-      @records[dictionary_key.to_s] = Hash.new
+      @get_records[dictionary_key.to_s] = Hash.new
 
-      filename = File.open('../dictionaries/' + dictionary_key.to_s + '.' + dictionary_value['format'].to_s)
-      delimiter = dictionary_value['delimiter'].to_i.chr(dictionary_value['encoding'].to_s)
+      if dictionary_value['format'] == 'txt'
+        filename = File.open('../dictionaries/' + dictionary_key.to_s + '.' + dictionary_value['format'].to_s)
+        delimiter = dictionary_value['delimiter'].to_i.chr(dictionary_value['encoding'].to_s)
 
-      filename_utf8 = Array.new
-      filename.each do |record|
-        filename_utf8.push record.to_s.encode('UTF-8', dictionary_value['encoding'].to_s)
-      end
+        filename_utf8 = Array.new
+        filename.each { |record|
+          filename_utf8.push record.to_s.encode('UTF-8', dictionary_value['encoding'].to_s)
+        }
 
-      index = 0
+        index = 0
 
-      filename_utf8.each do |line|
-        index += 1
-        @records[dictionary_key.to_s][index] = split_line(line, delimiter.encode('UTF-8'), dictionary_value['fields'])
+        filename_utf8.each { |line|
+          index += 1
+          @get_records[dictionary_key.to_s][index] = split_line(line, delimiter.encode('UTF-8'), dictionary_value['fields'])
+        }
       end
     end
 
-    @records
+    @get_records
   end
 
   def split_line(line, delimiter, fields_names)
@@ -38,10 +40,10 @@ class Dictionary
 
     index = 0
 
-    line.to_s.split(delimiter).each do |arr|
+    line.to_s.split(delimiter).each { |arr|
       index += 1
       @split_line[fields_names["column" + index.to_s]['name']] = arr
-    end
+    }
 
     @split_line
   end
