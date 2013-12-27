87186e0a275d7c7134ba9e723f258bab2b9d6b1e
Василий Потапов <potapov@che.sevcred.ru>
2013-12-27 13:34:39 +0400

    Fryazinovo parser done

--- app/dictionary.rb
+++ app/dictionary.rb
@@ -1,28 +1,53 @@
+require 'yaml'
+
 class Dictionary
 
-  def get_records(filename, delimiter)
+  def get_config(dict_name)
+    @get_config ||= YAML.load(File.read file_path(dict_name))
+  end
+
+  def get_records(dict_name)
     @records = Hash.new
 
-    index = 0
+    config = get_config(dict_name)
 
-    filename.each do |line|
-      index += 1
-      @records[index] = split_line(line, delimiter)
+    config['dictionaries'].each do |dictionary_key, dictionary_value|
+      @records[dictionary_key.to_s] = Hash.new
+
+      filename = File.open('../dictionaries/' + dictionary_key.to_s + '.' + dictionary_value['format'].to_s)
+      delimiter = dictionary_value['delimiter'].to_i.chr(dictionary_value['encoding'].to_s)
+
+      filename_utf8 = Array.new
+      filename.each do |record|
+        filename_utf8.push record.to_s.encode('UTF-8', dictionary_value['encoding'].to_s)
+      end
+
+      index = 0
+
+      filename_utf8.each do |line|
+        index += 1
+        @records[dictionary_key.to_s][index] = split_line(line, delimiter.encode('UTF-8'), dictionary_value['fields'])
+      end
     end
 
     @records
   end
 
-  def split_line(line, delimiter)
+  def split_line(line, delimiter, fields_names)
     @split_line = Hash.new
 
     index = 0
 
     line.to_s.split(delimiter).each do |arr|
       index += 1
-      @split_line["column" + index.to_s] = arr
+      @split_line[fields_names["column" + index.to_s]['name']] = arr
     end
 
     @split_line
   end
+
+  private
+    def file_path(dict_name)
+      "../conf/#{dict_name}/config.yml"
+    end
 end
--- app/read_dictionary.rb
+++ app/read_dictionary.rb
@@ -1,15 +1,10 @@
 require '../app/dictionary'
 
-dict_866 = File.open('../dictionaries/street.txt')
-dict_utf8 = Array.new
-
-dict_866.each do |record|
-  dict_utf8.push record.to_s.encode('UTF-8', 'cp866')
-end
-
 dict = Dictionary.new
 
-dict.get_records(dict_utf8, 179.chr('cp866').encode('UTF-8')).each  do |k,v|
+#puts dict.get_config('fryazinovo')
+
+dict.get_records('fryazinovo').each  do |k,v|
   v.each do |key, value|
     puts "#{k}, #{key}, #{value.to_s.encode('UTF-8')}"
   end
