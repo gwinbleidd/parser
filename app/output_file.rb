8fa5fb8c2361f2f717d83641a876a8955a4b534a
Василий Потапов <potapov@che.sevcred.ru>
2014-01-22 12:43:17 +0400

    Defined Start method for OutputFile

--- app/output_file.rb
+++ app/output_file.rb
@@ -2,13 +2,70 @@ require 'dictionary'
 
 module Dictionary
   class OutputFile
-    attr :output
-    def initialize(table)
-      mdls = Dictionary::Model.new(table)
+    attr_reader :output
 
-      mdls.main_view.all.each { |record|
+    def initialize(conf)
+      if conf.output_config['file'].has_key?('name') and conf.output_config['file'].has_key?('type')
+        @output_file = File.expand_path "../dictionaries/" + "output/#{conf.output_config['file']['name']}.#{conf.output_config['file']['type']}"
+      else
+        @output_file = nil
+      end
 
+      @config = conf
+
+      puts @output_file
+
+      if conf.output_config['file'].has_key?('name')
+        @sep = conf.output_config['file']['delimiter']
+      else @sep = nil
+      end
+    end
+
+    def start
+      mdls = Dictionary::Model.new(@config.table)
+
+      records = Array.new
+      mdls.main_view.all.each { |rec|
+        record = Hash.new
+        @config.output_config['fields'].each { |key, value|
+          if value['from'].is_a?(String)
+            record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_s.strip
+          elsif value['from'].is_a?(Array)
+            fields = Array.new
+            value['from'].each { |item| fields.push 'rec.' + item }
+            record[value['name'].to_sym] = eval fields.join "+\"#{value['delimiter']}\"+"
+          end
+        }
+
+        records.append record
       }
+
+      File.open(@output_file, "w+") do |csv|
+        csv.puts header(records.first)
+
+        records.each do |record|
+          csv.puts to_csv record
+        end
+      end
+    end
+
+    private
+    def output=(m)
+      @output = m
+    end
+
+    def header(hash)
+      hash.keys.join @sep
+    end
+
+    def to_csv(hash)
+      hash.values.map do |value|
+        escape value unless value.nil?
+      end.join @sep
+    end
+
+    def escape(string)
+      string.gsub(@sep, "")
     end
   end
 end
