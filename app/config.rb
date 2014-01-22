54daad90c6f90f8414e6addd07166555d8a48514
Василий Потапов <potapov@che.sevcred.ru>
2014-01-22 09:02:05 +0400

    Changed attr_accessor for attr_reader defining method=

--- app/config.rb
+++ app/config.rb
@@ -6,7 +6,7 @@ require 'digest/md5'
 
 module Dictionary
   class Config
-    attr_accessor :config, :output_config, :name, :primary_keys, :key_columns, :foreign_keys, :table
+    attr_reader :config, :output_config, :name, :primary_keys, :key_columns, :foreign_keys, :table
 
     def initialize(dict_name)
       dict_record = Dictionaries.find_by(:name => dict_name)
@@ -23,62 +23,62 @@ module Dictionary
         dict_record.save
       end
 
-      self.config||= YAML.load(File.read input_path(dict_name))
-      self.config['name']= dict_name
+      @config||= YAML.load(File.read input_path(dict_name))
+      @config['name']= dict_name
 
-      self.name = dict_name
+      @name = dict_name
 
-      self.primary_keys = get_primary_keys(self.name)
-      self.key_columns = get_key_columns(self.name)
-      self.foreign_keys = get_foreign_keys(self.name)
+      @primary_keys = get_primary_keys(@name)
+      @key_columns = get_key_columns(@name)
+      @foreign_keys = get_foreign_keys(@name)
 
-      self.output_config = get_output_config(dict_name)
+      @output_config = get_output_config(dict_name)
 
-      self.table = get_table
+      @table = get_table
     end
 
     private
     def get_table
-      unless self.config.nil?
+      unless @config.nil?
         @get_table = Hash.new
 
-        self.config['dictionaries'].each { |key, value|
+        @config['dictionaries'].each { |key, value|
           table = Hash.new
-          table[:dictionary] = self.name.to_s.downcase
+          table[:dictionary] = @name.to_s.downcase
           table[:main] = value['main'] if value.has_key?('main')
           table[:fields] = Hash.new
           table[:fields] = value['fields']
 
-          if self.primary_keys.has_key?(key)
+          if @primary_keys.has_key?(key)
             table[:pk] = Hash.new
-            table[:pk] = self.primary_keys[key][:pk].to_s
+            table[:pk] = @primary_keys[key][:pk].to_s
           end
 
-          if self.foreign_keys.has_key?(key)
+          if @foreign_keys.has_key?(key)
             unless table.has_key?(:fk)
               table[:fk] = Hash.new
             end
 
-            self.foreign_keys[key].each { |k, v|
+            @foreign_keys[key].each { |k, v|
               table[:fk][k] = Hash.new
               table[:fk][k][:column] = v[:column].to_s
-              table[:fk][k][:table] = self.name.to_s.downcase + '_' + v[:table].to_s.downcase
+              table[:fk][k][:table] = @name.to_s.downcase + '_' + v[:table].to_s.downcase
               table[:fk][k][:column_ref] = v[:column_ref].to_s
               table[:fk][k][:return] = v[:return].to_s
             }
           end
 
-          if self.key_columns.has_key?(key)
+          if @key_columns.has_key?(key)
             unless table.has_key?(:ak)
               table[:ak] = Hash.new
             end
 
-            self.key_columns[key].each { |k, v|
+            @key_columns[key].each { |k, v|
               table[:ak][k] = v
             }
           end
 
-          @get_table[(self.name.to_s.downcase + '_' + key.to_s.downcase).to_sym] = table
+          @get_table[(@name.to_s.downcase + '_' + key.to_s.downcase).to_sym] = table
         }
 
         @get_table
@@ -91,15 +91,15 @@ module Dictionary
     end
 
     def get_foreign_keys(dict_name)
-      if self.name == nil
-        self.name= dict_name
+      if @name.nil?
+        @name= dict_name
       end
 
-      if self.config == nil
-        self.config= get_config(self.name)
+      if @config.nil?
+        @config= get_config(@name)
       end
 
-      self.config['dictionaries'].each { |key, value|
+      @config['dictionaries'].each { |key, value|
         i = 0
 
         value['fields'].each { |k, v|
@@ -129,15 +129,15 @@ module Dictionary
     end
 
     def get_primary_keys(dict_name)
-      if self.name == nil
-        self.name= dict_name
+      if @name.nil?
+        @name = dict_name
       end
 
-      if self.config == nil
-        self.config= get_config(self.name)
+      if @config.nil?
+        @config= get_config(@name)
       end
 
-      self.config['dictionaries'].each { |key, value|
+      @config['dictionaries'].each { |key, value|
         value['fields'].each { |k, v|
           if v['pk']
             if @get_primary_keys == nil
@@ -160,17 +160,17 @@ module Dictionary
     end
 
     def get_key_columns(dict_name)
-      if self.name == nil
-        self.name= dict_name
+      if @name.nil?
+        @name= dict_name
       end
 
-      if self.config == nil
-        self.config= get_config(self.name)
+      if @config.nil?
+        @config= get_config(@name)
       end
 
       @get_key_columns = Hash.new
 
-      self.config['dictionaries'].each { |key, value|
+      @config['dictionaries'].each { |key, value|
         @get_key_columns[key] = Hash.new
 
         value['fields'].each { |k, v|
@@ -192,6 +192,34 @@ module Dictionary
 
     def output_path(dict_name)
       "../config/#{dict_name}/output.yml"
+    end
+
+    def config=(m)
+      @config = m
+    end
+
+    def output_config=(m)
+      @output_config = m
+    end
+
+    def name=(m)
+      @name = m
+    end
+
+    def primary_keys=(m)
+      @primary_keys = m
+    end
+
+    def key_columns=(m)
+      @key_columns = m
+    end
+
+    def foreign_keys=(m)
+      @foreign_keys = m
+    end
+
+    def table=(m)
+      @table = m
     end
   end
 end
