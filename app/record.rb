0e2d3c81b167dbcf4ebd53d8d223959632079773
Василий Потапов <potapov@che.sevcred.ru>
2014-01-24 12:29:05 +0400

    Changed class for set fof files

--- app/record.rb
+++ app/record.rb
@@ -4,23 +4,36 @@ module Dictionary
 
     def initialize(config)
       @records = Hash.new
+      filename = nil
 
-      config['dictionaries'].each do |dictionary_key, dictionary_value|
-        @records[dictionary_key.to_s.to_sym] = Hash.new
+      dict_name = config['name']
 
-        dict_name = config['name']
+      paths = Pathname.new(File.expand_path('../.') + '/dictionaries/' + dict_name).children.select { |c| c.directory? }.collect { |p| p.to_s }
 
-        if dictionary_value['format'] == 'txt'
-          filename = File.open('../dictionaries/' + dict_name + '/' + dictionary_key.to_s + '.txt')
-          delimiter = dictionary_value['delimiter'].to_i.chr(dictionary_value['encoding'].to_s)
+      paths.each do |path|
+        @records[File.basename path.gsub('_', '.')] = Hash.new
 
-          index = 0
+        config['dictionaries'].each do |dictionary_key, dictionary_value|
+          @records[File.basename path.gsub('_', '.')][dictionary_key.to_s.to_sym] = Hash.new
 
-          filename.each { |line|
-            index += 1
-            line = line.to_s.encode('UTF-8', dictionary_value['encoding'].to_s).delete("\r\n")
-            @records[dictionary_key.to_s.to_sym][index] = is_record(line, delimiter.encode('UTF-8'), dictionary_value['fields'])
-          }
+          if dictionary_value['format'] == 'txt'
+            if File.exist?(path + '/' + dictionary_key.to_s + '.txt')
+              filename = File.open(path.to_s + '/' + dictionary_key.to_s + '.txt')
+            else
+              puts "File #{path.to_s + '/' + dictionary_key.to_s}.txt doesn\'t exist"
+              exit
+            end
+
+            delimiter = dictionary_value['delimiter'].to_i.chr(dictionary_value['encoding'].to_s)
+
+            index = 0
+
+            filename.each do |line|
+              index += 1
+              line = line.to_s.encode('UTF-8', dictionary_value['encoding'].to_s).delete("\r\n")
+              @records[File.basename path.gsub('_', '.')][dictionary_key.to_s.to_sym][index] = is_record(line, delimiter.encode('UTF-8'), dictionary_value['fields'])
+            end
+          end
         end
       end
     end
