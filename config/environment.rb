1714f01748a529db35ee057bd4311bf4e57a5321
Василий Потапов <potapov@che.sevcred.ru>
2014-02-05 16:56:33 +0400

    Changed ActiveRecord Logger level to WARN

--- config/environment.rb
+++ config/environment.rb
@@ -24,7 +24,7 @@ dbconfig = YAML::load(File.open(File.join(File.dirname(__FILE__), 'database.yml'
 
 # Ошибки работы с БД направим в стандартный поток (консоль)
 ActiveRecord::Base.logger = Logger.new(File.expand_path(File.join(File.dirname(__FILE__), "../log/db.log"))) # Simple logging utility. logger.rb -- standart lib
-#ActiveRecord::Base.logger.level = Logger::WARN
+ActiveRecord::Base.logger.level = Logger::WARN
 
 # Соединяемся с БД
 ActiveRecord::Base.establish_connection(dbconfig)
