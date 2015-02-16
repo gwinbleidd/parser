9e0d8e1ddba86f2a4032a8b2c17c1a5d5fe2ddfb
Василий Потапов <potapov@che.sevcred.ru>
2015-02-16 17:19:42 +0300

    Some changes due to new Primary key data structure configuration

--- db/dictionary_uniq_const_migration.rb
+++ db/dictionary_uniq_const_migration.rb
@@ -4,12 +4,12 @@ class DictionaryUniqConstMigration < ActiveRecord::Migration
       Dictionary.logger.debug "Creating unique index for #{key}"
       if value.has_key?(:pk)
         if ActiveRecord::Base.connection.index_exists? key.to_s.pluralize.to_sym, value[:pk], unique: true
-          Dictionary.logger.debug " Index for #{value[:pk]} exists"
+          Dictionary.logger.debug " Index for #{value[:pk][:name]} exists"
         else
-          Dictionary.logger.debug " Index for #{value[:pk]} doesn\'t exists"
+          Dictionary.logger.debug " Index for #{value[:pk][:name]} doesn\'t exists"
         end
         unless ActiveRecord::Base.connection.index_exists? key.to_s.pluralize.to_sym, value[:pk], unique: true
-          add_index key.to_s.pluralize.to_sym, value[:pk], unique: true
+          add_index key.to_s.pluralize.to_sym, value[:pk][:name], unique: true
         end
       end
     end
@@ -18,7 +18,7 @@ class DictionaryUniqConstMigration < ActiveRecord::Migration
   def self.down(table)
     table.each do |key, value|
       if value.has_key?(:pk)
-        remove_index key.to_s.pluralize.to_sym, value[:pk]
+        remove_index key.to_s.pluralize.to_sym, value[:pk][:name]
       end
     end
   end
