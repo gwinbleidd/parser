4af2c9bfcc3784a23d1d4072efa3393a6c3cd9b0
Василий Потапов <potapov@che.sevcred.ru>
2015-02-17 17:24:12 +0300

    Changed due to new configuration

--- lib/output.rb
+++ lib/output.rb
@@ -14,32 +14,45 @@ module Dictionary
           output[:data][id] = Hash.new
 
           config.output_config['fields'].each do |cid, cdesc|
-            case cdesc['from'].class.to_s
-              when 'String' then
-                output[:data][id][cdesc['name']] = rdata[cdesc['from'].to_sym] unless cdesc['from'] == 'id'
-              when 'Array'
-                tmp = Hash[(0...cdesc['from'].size).zip cdesc['from']]
-                str = ''
-                tmp.each do |k, v|
-                  if k != cdesc['from'].index(cdesc['from'].last)
-                    str << rdata[v.to_sym] + cdesc['delimiter']
-                  else
-                    str << rdata[v.to_sym]
-                  end
-                end
-
-                output[:data][id][cdesc['name']] = str
-              when 'NilClass'
-                if cdesc.has_key?('const')
-                  output[:data][id][cdesc['name']] = cdesc['const']
-                else
-                  raise "Unknown field type with NilClass"
-                end
-              else
-                puts "#{cdesc['from'].class.to_s} - #{cdesc['from']}"
-                raise "Unknown class"
+            if cdesc.has_key?('const')
+              output[:data][id][cdesc['name']] = cdesc['const']
             end
 
+            if cdesc.has_key?('from')
+              case cdesc['from'].class.to_s
+                when 'String' then
+                  if cdesc.has_key?('replace')
+                    cdesc['replace'].each do |k, v|
+                      if rdata[cdesc['from'].to_sym].to_s == v.to_s
+                        output[:data][id][cdesc['name']] = k.to_s
+                      end
+                    end
+                  else
+                    output[:data][id][cdesc['name']] = rdata[cdesc['from'].to_sym] unless cdesc['from'] == 'id'
+                  end
+                when 'Array'
+                  tmp = Hash[(0...cdesc['from'].size).zip cdesc['from']]
+                  str = ''
+                  tmp.each do |k, v|
+                    if k != cdesc['from'].index(cdesc['from'].last)
+                      str << rdata[v.to_sym] + cdesc['delimiter']
+                    else
+                      str << rdata[v.to_sym]
+                    end
+                  end
+
+                  output[:data][id][cdesc['name']] = str
+                when 'NilClass'
+                  if cdesc.has_key?('const')
+                    output[:data][id][cdesc['name']] = cdesc['const']
+                  else
+                    raise "Unknown field type with NilClass"
+                  end
+                else
+                  puts "#{cdesc['from'].class.to_s} - #{cdesc['from']}"
+                  raise "Unknown class"
+              end
+            end
           end
         end
       end
