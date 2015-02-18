7c1d557a21fdf0bac6677fd8e84ecdc6b2c76081
Василий Потапов <potapov@che.sevcred.ru>
2015-02-18 10:22:14 +0300

    Some changes for logger

--- lib/output.rb
+++ lib/output.rb
@@ -3,6 +3,7 @@ module Dictionary
     attr_reader :records
 
     def initialize(config, records)
+      Dictionary.logger.info("Starting create output array of data for #{config.name}")
       output = Hash.new
 
       records.each do |key, data|
