9f9229232ad5d4d6afe4c8c771fefca0584b9ddc
Василий Потапов <potapov@che.sevcred.ru>
2015-02-18 11:29:01 +0300

    Changes due to new configuration of objects

--- lib/output_file.rb
+++ lib/output_file.rb
@@ -14,41 +14,32 @@ module Dictionary
 
       if conf.output_config['file'].has_key?('name')
         @sep = conf.output_config['file']['delimiter']
-      else @sep = nil
+      else
+        @sep = nil
       end
     end
 
     def start(mdls)
       records = Array.new
-      mdls.main_view.all.each do |rec|
-        record = Hash.new
-        @config.output_config['fields'].each { |key, value|
-          if value['from'].is_a?(String)
-            if value.has_key?('type')
-              record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_i.to_s if value['type'] == 'number'
-              record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_f.to_s if value['type'] == 'currency'
-              record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_f.to_s if value['type'] == 'float'
+      mdls.objects.each do |dict|
+        dict.all.each do |rec|
+          record = Hash.new
+          @config.output_config['fields'].each { |key, value|
+            if value['from'] == 'id'
+              record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_i.to_s
             else
-              record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_s.strip.encode(@config.output_config['file']['encoding'])
-            end
-
-            if value.has_key?('replace')
-              value['replace'].each do |k,v|
-                if eval('rec.' + value['from'].to_s).to_s.strip.encode(@config.output_config['file']['encoding']) == v.to_s.strip.encode(@config.output_config['file']['encoding'])
-                  record[value['name'].to_sym] = k.to_s.strip.encode(@config.output_config['file']['encoding'])
-                end
+              if value.has_key?('type')
+                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_i.to_s if value['type'] == 'number'
+                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_f.to_s if value['type'] == 'currency'
+                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_f.to_s if value['type'] == 'float'
+              else
+                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_s.strip.encode(@config.output_config['file']['encoding'])
               end
             end
-          elsif value['from'].is_a?(Array)
-            fields = Array.new
-            value['from'].each { |item| fields.push eval('rec.' + item).to_s.encode("UTF-8") }
-            record[value['name'].to_sym] = fields.join("#{value['delimiter']}").to_s.strip.encode(@config.output_config['file']['encoding'])
-          elsif value.has_key?('const')
-            record[value['name'].to_sym] = value['const'].to_s
-          end
-        }
+          }
 
-        records.append record
+          records.append record
+        end
       end
 
       File.open(@output_file, "w+") do |csv|
