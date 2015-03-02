408ba601094ee123485fcb049cab21cf315c76c3
Василий Потапов <potapov@che.sevcred.ru>
2015-03-02 13:57:56 +0300

    Some refactor

--- lib/output_file.rb
+++ lib/output_file.rb
@@ -20,33 +20,58 @@ module Dictionary
     end
 
     def start(mdls)
-      records = Array.new
-      mdls.objects.each do |dict|
-        dict.all.each do |rec|
-          record = Hash.new
-          @config.output_config['fields'].each { |key, value|
-            if value['from'] == 'id'
-              record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_i.to_s
+      File.open(@output_file, "w") do |csv|
+        i = 0
+
+        mdls.objects.each do |dict|
+          dict.all.nil? ? size = 0 : size = dict.all.size
+
+          case size
+            when 0 then
+              raise "No data for #{dict.to_s}"
+            when 1 .. 10 then
+              mod = 1
+            when 11 .. 1000 then
+              mod = 5
+            when 1001 .. 10000 then
+              mod = 100
+            when 10001 .. 50000 then
+              mod = 500
             else
-              if value.has_key?('type')
-                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_i.to_s if value['type'] == 'number'
-                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_f.to_s if value['type'] == 'currency'
-                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_f.to_s if value['type'] == 'float'
+              mod = 1000
+          end
+
+          dict.all.each do |rec|
+            record = Hash.new
+            @config.output_config['fields'].each { |key, value|
+              if value['from'] == 'id'
+                record[value['name'].to_sym] = eval('rec.' + value['from'].to_s).to_i.to_s
               else
-                record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_s.strip.encode(@config.output_config['file']['encoding'])
+                if value.has_key?('type')
+                  record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_i.to_s if value['type'] == 'number'
+                  record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_f.to_s if value['type'] == 'currency'
+                  record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_f.to_s if value['type'] == 'float'
+                else
+                  record[value['name'].to_sym] = eval('rec.' + value['name'].to_s).to_s.strip.encode(@config.output_config['file']['encoding'])
+                end
               end
+            }
+
+            if i == 0
+              csv.write header(record)
+              csv.write to_csv record
+            else
+              csv.write to_csv record
             end
-          }
 
-          records.append record
-        end
-      end
+            i+=1
 
-      File.open(@output_file, "w+") do |csv|
-        csv.puts header(records.first)
-
-        records.each do |record|
-          csv.puts to_csv record
+            if i == size
+              Dictionary.logger.info("#{dict.to_s}: Outputed #{i} of #{size} records")
+            else
+              print "Outputed #{i} of #{size} records\r" if i % mod == 0 or i == 1
+            end
+          end
         end
       end
     end
