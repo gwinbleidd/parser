b62a5c5fa67a0c6c36dfe1b7fd5d14fc7f1d2e9b
Василий Потапов <potapov@che.sevcred.ru>
2015-04-09 15:33:40 +0300

    Logger changes

--- db/dictionary_uniq_const_migration.rb
+++ db/dictionary_uniq_const_migration.rb
@@ -1,51 +1,47 @@
 class DictionaryUniqConstMigration < ActiveRecord::Migration
   def self.up(table)
-    table.each do |key, value|
-      Dictionary.logger.debug "Creating unique index for #{key}"
-      if value.has_key?(:pk)
-        if ActiveRecord::Base.connection.index_exists? key.to_s.pluralize.to_sym, value[:pk], unique: true
-          Dictionary.logger.debug " Index for #{value[:pk][:name]} exists"
-        else
-          Dictionary.logger.debug " Index for #{value[:pk][:name]} doesn\'t exists"
-        end
-        unless ActiveRecord::Base.connection.index_exists? key.to_s.pluralize.to_sym, value[:pk], unique: true
-          add_index key.to_s.pluralize.to_sym, value[:pk][:name], unique: true
-        end
+    $log.debug "Creating unique index for #{table[:name]}"
+    if table.has_key?(:pk)
+      if ActiveRecord::Base.connection.index_exists? table[:name], table[:pk], unique: true
+        $log.debug " Index for #{table[:pk][:name]} exists"
+      else
+        $log.debug " Index for #{table[:pk][:name]} doesn\'t exists"
       end
+      unless ActiveRecord::Base.connection.index_exists? table[:name], table[:pk], unique: true
+        add_index table[:name], table[:pk][:name], unique: true
+      end
+    end
 
-      if value.has_key?(:keys)
-        keys = Array.new
-        value[:keys].each do |key_column|
-          keys.append key_column[:name]
-        end
-        if ActiveRecord::Base.connection.index_exists? key.to_s.pluralize.to_sym, keys, unique: true
-          Dictionary.logger.debug " Index for #{keys} exists"
-        else
-          Dictionary.logger.debug " Index for #{keys} doesn\'t exists"
-        end
-        unless ActiveRecord::Base.connection.index_exists? key.to_s.pluralize.to_sym, keys, unique: true
-          add_index key.to_s.pluralize.to_sym, keys, unique: true
-        end
+    if table.has_key?(:keys)
+      keys = Array.new
+      table[:keys].each do |key_column|
+        keys.append key_column[:name]
+      end
+      if ActiveRecord::Base.connection.index_exists? table[:name], keys, unique: true
+        $log.debug " Index for #{keys} exists"
+      else
+        $log.debug " Index for #{keys} doesn\'t exists"
+      end
+      unless ActiveRecord::Base.connection.index_exists? table[:name], keys, unique: true
+        add_index table[:name], keys, unique: true
       end
     end
   end
 
   def self.down(table)
-    table.each do |key, value|
-      if value.has_key?(:pk)
-        remove_index key.to_s.pluralize.to_sym, value[:pk][:name]
-      end
+    if table.has_key?(:pk)
+      remove_index table[:name], table[:pk][:name]
+    end
 
-      if value.has_key?(:keys)
-        keys = Array.new
-        value[:keys].each do |k, v|
-          v.each do |key_column|
-            keys.append key_column[:name]
-          end
+    if table.has_key?(:keys)
+      keys = Array.new
+      table[:keys].each do |k, v|
+        v.each do |key_column|
+          keys.append key_column[:name]
         end
-
-        remove_index key.to_s.pluralize.to_sym, keys, unique: true
       end
+
+      remove_index table[:name], keys, unique: true
     end
   end
 end
