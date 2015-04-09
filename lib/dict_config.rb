7d928d4caeefd5e6f987527d5d4820edb95cfb57
Василий Потапов <potapov@che.sevcred.ru>
2015-04-09 15:42:41 +0300

    Some changes

--- lib/dict_config.rb
+++ lib/dict_config.rb
@@ -1,34 +1,40 @@
+require 'config_file'
+
 class DictConfig
-  def input_config(dict_name)
-    @input_config||= YAML.load(File.read input_path(dict_name))
+  attr_reader :name, :file_properties
+
+  def initialize(dict_name)
+    @name = dict_name
+    @cf = ConfigFile.new
+    @file_properties = @cf.config[@name]
   end
 
-  def output_config(dict_name)
-    @output_config||= YAML.load(File.read output_path(dict_name))
+  def input_config
+    @input_config||= YAML.load(File.read input_path(self.name))
   end
 
-  def upload_config(dict_name)
-    dict_record = Dictionaries.find_by(:name => dict_name)
+  def output_config
+    @output_config||= YAML.load(File.read output_path(self.name))
+  end
+
+  def upload_config
+    dict_record = Dictionaries.find_by(:name => self.name)
     if dict_record == nil
-      Dictionaries.create(:name => dict_name,
-                          :input_config => File.read(input_path(dict_name)),
-                          :input_config_md5 => Digest::MD5.file(input_path(dict_name)).hexdigest.to_s,
-                          :output_config => File.read(output_path(dict_name)),
-                          :output_config_md5 => Digest::MD5.file(output_path(dict_name)).hexdigest.to_s)
-    elsif dict_record.input_config_md5 != Digest::MD5.file(input_path(dict_name)).hexdigest.to_s
-      dict_record.update(:input_config => File.read(input_path(dict_name)),
-                         :input_config_md5 => Digest::MD5.file(input_path(dict_name)).hexdigest.to_s)
-    elsif dict_record.output_config_md5 != Digest::MD5.file(output_path(dict_name)).hexdigest.to_s
-      dict_record.update(:output_config => File.read(output_path(dict_name)),
-                         :output_config_md5 => Digest::MD5.file(output_path(dict_name)).hexdigest.to_s)
+      Dictionaries.create(:name => self.name,
+                          :input_config => File.read(input_path(self.name)),
+                          :input_config_md5 => Digest::MD5.file(input_path(self.name)).hexdigest.to_s,
+                          :output_config => File.read(output_path(self.name)),
+                          :output_config_md5 => Digest::MD5.file(output_path(self.name)).hexdigest.to_s)
+    elsif dict_record.input_config_md5 != Digest::MD5.file(input_path(self.name)).hexdigest.to_s
+      dict_record.update(:input_config => File.read(input_path(self.name)),
+                         :input_config_md5 => Digest::MD5.file(input_path(self.name)).hexdigest.to_s)
+    elsif dict_record.output_config_md5 != Digest::MD5.file(output_path(self.name)).hexdigest.to_s
+      dict_record.update(:output_config => File.read(output_path(self.name)),
+                         :output_config_md5 => Digest::MD5.file(output_path(self.name)).hexdigest.to_s)
     end
   end
 
   private
-  def validate(config)
-    config
-  end
-
   def input_path(dict_name)
     "../config/#{dict_name}/input.yml"
   end
