d1ec77d0d737cc16880f730034bee556729e40d7
Василий Потапов <potapov@che.sevcred.ru>
2015-04-09 12:00:19 +0300

    Errors in uploading config files, Hash is not able to upload to database

--- lib/config_file.rb
+++ lib/config_file.rb
@@ -21,15 +21,15 @@ class ConfigFile
     dict_record = Dictionaries.find_by(:name => dict_name)
     if dict_record == nil
       Dictionaries.create(:name => dict_name,
-                          :input_config => input_config(dict_name),
+                          :input_config => File.read(input_path(dict_name)),
                           :input_config_md5 => Digest::MD5.file(input_path(dict_name)).hexdigest.to_s,
-                          :output_config => output_config(dict_name),
+                          :output_config => File.read(output_path(dict_name)),
                           :output_config_md5 => Digest::MD5.file(output_path(dict_name)).hexdigest.to_s)
     elsif dict_record.input_config_md5 != Digest::MD5.file(input_path(dict_name)).hexdigest.to_s
-      dict_record.update(:input_config => input_config(dict_name),
+      dict_record.update(:input_config => File.read(input_path(dict_name)),
                          :input_config_md5 => Digest::MD5.file(input_path(dict_name)).hexdigest.to_s)
     elsif dict_record.output_config_md5 != Digest::MD5.file(output_path(dict_name)).hexdigest.to_s
-      dict_record.update(:output_config => output_config(dict_name),
+      dict_record.update(:output_config => File.read(output_path(dict_name)),
                          :output_config_md5 => Digest::MD5.file(output_path(dict_name)).hexdigest.to_s)
     end
   end
