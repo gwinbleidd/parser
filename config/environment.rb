cc4433b737c7e62714126961688342797092a344
Василий Потапов <potapov@che.sevcred.ru>
2015-04-10 17:32:44 +0300

    Some changes

--- config/environment.rb
+++ config/environment.rb
@@ -20,17 +20,30 @@ require 'dictionary_view_migration'
 require 'multi_io'
 require 'output'
 require 'joined'
+require 'logger'
 
 ENV['PATH'] = "D:\\oracle\\instantclient_11_2;#{ENV['PATH']}"
 ENV['NLS_LANG']="AMERICAN_CIS.CL8MSWIN1251"
-ENV['ENV'] = 'production'
+ENV['ENV'] = ENV['ENV']||'development'
 
 # Загружаем файл настройки соединения с БД
 dbconfig = YAML::load(File.open(File.join(File.dirname(__FILE__), 'database.yml')))
 
 # Ошибки работы с БД направим в стандартный поток (консоль)
 ActiveRecord::Base.logger = Logger.new(File.expand_path(File.join(File.dirname(__FILE__), "../log/db.log"))) # Simple logging utility. logger.rb -- standart lib
-ActiveRecord::Base.logger.level = Logger::INFO
+ActiveRecord::Base.logger.level = Logger::DEBUG
 
 # Соединяемся с БД
 ActiveRecord::Base.establish_connection(dbconfig[ENV['ENV']])
+
+log_file = File.open("../log/#{ENV['ENV']}.log", "a+")
+$log = Logger.new MultiIO.new(STDOUT, log_file)
+$log.level = Logger::DEBUG
+$log.formatter = proc do |severity, datetime, progname, msg|
+  date_format = datetime.strftime("%d.%m.%Y %H:%M:%S")
+  if severity == "INFO" or severity == "WARN"
+    "[#{date_format}] #{severity}: #{msg}\n"
+  else
+    "[#{date_format}] #{severity}: #{msg}\n"
+  end
+end
