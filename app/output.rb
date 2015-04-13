5658802c6675cf528eb6695cc6b40c0a06f1a178
Василий Потапов <potapov@che.sevcred.ru>
2015-04-13 10:37:41 +0300

    Some changes

--- app/output.rb
+++ app/output.rb
@@ -1,14 +1,6 @@
 # encoding: utf-8
 require File.join(File.dirname(__FILE__), '../config/environment.rb')
 
-ENV['ENV'] = 'development'
-
-require 'rubygems'
-require 'active_record'
-require 'yaml'
-require 'logger'
-require '../lib/configuration'
-
 conf = Configuration.new('mkd')
 models = Model.new(conf.table[:dictionary])
 out = OutputFile.new(conf)
