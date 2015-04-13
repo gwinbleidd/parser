88571e86659f6605184ec8cee999f01463a8f585
Василий Потапов <potapov@che.sevcred.ru>
2015-04-13 11:10:42 +0300

    Logger changes

--- db/dictionary_table_migration.rb
+++ db/dictionary_table_migration.rb
@@ -1,11 +1,11 @@
 class DictionaryTableMigration < ActiveRecord::Migration
   def self.up(name, fields)
     unless ActiveRecord::Base.connection.table_exists?(name.to_s.to_sym)
-      Logger.logger.debug "Creating table #{name.to_s.to_sym}, #{fields}"
+      $log.debug "Creating table #{name.to_s.to_sym}, #{fields}"
       create_table name.to_s.to_sym
 
       fields.each do |key, value|
-        Logger.logger.debug " Adding column #{value['name'].to_s.to_sym}: #{value['type'].to_s.to_sym}"
+        $log.debug " Adding column #{value['name'].to_s.to_sym}: #{value['type'].to_s.to_sym}"
         unless ActiveRecord::Base.connection.column_exists?(name.to_s.to_sym, value['name'].to_s.to_sym, value['type'].to_s.to_sym)
           add_column name.to_s.to_sym, value['name'].to_s.to_sym, value['type'].to_s.to_sym
         end
