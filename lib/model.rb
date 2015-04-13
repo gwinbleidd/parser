9d3a531b443117aec7bfe78116040aaeba9b79c9
Василий Потапов <potapov@che.sevcred.ru>
2015-04-13 15:49:38 +0300

    Moved Objects method in initialize for no reuse of them

--- lib/model.rb
+++ lib/model.rb
@@ -2,17 +2,15 @@ require 'config_file'
 require 'configuration'
 
 class Model
-  attr_reader :config, :name
+  attr_reader :config, :name, :objects
 
   def initialize(dict_name)
     cf = ConfigFile.new
     @name = dict_name
     @config = Configuration.new(@name)
-  end
 
-  def objects
     attrs = Array.new
-    objects = Array.new
+    @objects = Array.new
 
     @config.table[:fields].each { |field_name, field_def|
       attrs.push field_def['name'].to_sym
@@ -28,8 +26,6 @@ class Model
 
     eval "#{@name} = #{class_name}.new" or abort "Class instantiation failed"
 
-    objects.append obj
-
-    objects
+    @objects.append obj
   end
 end
